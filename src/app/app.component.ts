baab127f77f4835c05d86101a64231c55a92768b
Katya <konovalova.ep@prg-it.ru>
2021-10-07 15:09:43 +0500

    немного контента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component/* , OnInit */ } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent/*  implements OnInit */{
   title="ng-basics"
+  name ="value"
 
+  toggle=false
+  toggle2=false
+  toggle3=false
+  
+  addInput(name:string) {
+    console.log(this.name)
 }
+  toggleCards(){
+    this.toggle=!this.toggle
+  }
+  toggleCards2(){
+    this.toggle2=!this.toggle2
+  }
+  toggleCards3(){
+    this.toggle3=!this.toggle3
+  }
+}
+
