2cfedc39aab905a57f4c481536dcf72ff1cbfead
Katya <konovalova.ep@prg-it.ru>
2021-10-11 15:06:28 +0500

    шапка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component/* , OnInit */ } from '@angular/core';
+import { Component/* , OnInit  */ } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent/*  implements OnInit */{
+export class AppComponent/*  implements OnInit */ {
   title="ng-basics"
   name ="value"
- 
+  imgDjo="https://avatars.mds.yandex.net/get-ott/1534341/2a00000176f15a40fc3ec20b6a61c0b19850/960x540"
+  imgUrl="https://thumbs.dfs.ivi.ru/storage33/contents/1/e/0e19422d9f0ce8eccacd481bd985a2.jpg"
+
+  /* ngOnInit() {
+    setInterval ( () => {
+      this.imgUrl='https://avatars.mds.yandex.net/get-ott/1534341/2a0000016f0e2ff213c7112837259eaa4b4e/1344x756'
+    }, 3000)
+    }   */
   addInput(name:string) {
     console.log(this.name)
 }
+
  /*  toggleCards(){
     this.toggle=!this.toggle
   }
@@ -21,5 +29,8 @@ export class AppComponent/*  implements OnInit */{
   toggleCards3(){
     this.toggle3=!this.toggle3
   } */
+
+    
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent ]
