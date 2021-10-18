eff6aee624412ece9188edf0261b8e987f054cb2
Philipp Kazhin <kazhin@prg-it.ru>
2021-10-18 12:36:08 +0500

    feat: Add splider for example

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import Splide from '@splidejs/splide';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css'],
 })
-export class ShopComponent implements OnInit {
+export class ShopComponent implements OnInit, AfterViewInit {
   constructor() {}
 
   ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    new Splide( '.splide' ).mount();
+  }
 }
